15d81459a87cec5af54575595f2f63adce083632
Mikkita1 <152536723+Mikkita1@users.noreply.github.com>
2024-04-25 18:08:08 +0200

    Create Consultas-monotabla.sql

---
+++ Consultas-monotabla.sql
@@ -0,0 +1,7 @@
+-- Crea 2 consultas monotabla que se ejecuten regularmente en la aplicación.
+
+SELECT * FROM mascota WHERE especie="perro";
+-- Nos muestra los datos de las mascotas que sean perros
+
+SELECT * FROM citas WHERE fecha='2024-04-%';
+-- Nos muetra las citas de este mes
