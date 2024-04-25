3ef533a47f354c2557944b78f8cadc68fc69a3c3
Mikkita1 <152536723+Mikkita1@users.noreply.github.com>
2024-04-25 17:58:56 +0200

    Update Diseño-BBDD.sql

--- Diseño-BBDD.sql
+++ Diseño-BBDD.sql
@@ -4,6 +4,16 @@ CREATE DATABASE IF NOT EXIST CitasVeterinarias;
 
 USE CitasVeterinarias;
 
+CREATE TABLE cliente (
+  dni int(8) primary key not null,
+  nombre varchar(25),
+  apellido1 varchar(25),
+  apellido2 varchar(25),
+  direccion varchar(100),
+  telefono int(9),
+  email varchar(50)
+);
+
 CREATE TABLE mascota (
   id_mascota int auto_increment primary key not null,
   nombre varchar(25),
@@ -11,43 +21,35 @@ CREATE TABLE mascota (
   raza varchar(25),
   genero varchar(10),
   color varchar(25),
-  fecha_nacimiento date
-);
-
-CREATE TABLE cliente (
-  id_cliente int auto_increment primary key not null,
-  nombre varchar(25),
-  apellido1 varchar(25),
-  apellido2 varchar(25),
-  direccion varchar(100),
-  telefono int(9),
-  email varchar(50),
-  foreign key (id_mascota) references mascota(id_mascota)
+  fecha_nacimiento date,
+  CONSTRAINT dni_propietario foreign key (dni) references cliente(dni)
 );
 
 CREATE TABLE citas (
   num_cita int auto_increment primary key not null,
   fecha date,
   hora time,
-  foreign key (id_cliente) references cliente(id_cliente),
   foreign key (id_mascota) references mascota(id_mascota)
 );
 
--- Datos de la tabla mascota
-INSERT INTO mascota (nombre, especie, raza, genero, color, fecha_nacimiento) VALUES
-("Angy", "perro", "chihuahua", "hembra", "marron", '2011-10-17'),
-("Lily", "perro", "bulldog frances", "hembra", "blanco y negro", '2015-04-01'),
-("Georgia", "perro", "chihuahua", "hembra", "blanco y marron", '2019-11-15'),
-("Gnar", "gato", "birmano", "macho", "beis y marron", '2021-03-23'),
-("Coco", "dragon barbudo", "pogona", "hembra", "crema", '2018-09-12'),
-("Peluso", "huron", "americano", "macho", "beis", '2008-07-03');
+-- Datos de la tabla cliente
+INSERT INTO clientes VALUES
+(05674382, "Luis", "Gerard", "Montes", "calle cordoba, 14", 912334567, "luisgm@mail.com"),
+(03748519, "Marta", "Garcia", "Lopez", "avenida andalucia, 23 4ºa", 651234098, "martitalagl@mail.es"),
+(02391647, "Ana", "Morales", "Cordoba", "calle del cerro, 32 1ºc", 732451872, "anitamora@mail.com"),
+(05862184, "Adan", "Gonzalez", "Ruiz", "calle del olvido, 1", 654712330, "adan87grrr@mail.com"),
+(02648391, "Maria", "Lopez", "Perez", "avenida buena vista, 33", 675000981, "lamarilo@mail.com"),
+(03657789, "Lola", "Diaz", "Adalbert", "calle cristo redentor, 46 2ºb", 700123665, "lolitalamasbonita@mail.com");
 
-INSERT INTO clientes (nombre, apellido1, apellido2, direccion, telefono, email) VALUES
-("Luis", "Gerard", "Montes", "calle cordoba, 14", 912334567, "luisgm@mail.com"),
-("Marta", "Garcia", "Lopez", "avenida andalucia, 23 4ºa", 651234098, "martitalagl@mail.es"),
-("Ana", "Morales", "Cordoba", "calle del cerro, 32 1ºc", 732451872, "anitamora@mail.com"),
-("Adan", "Gonzalez", "Ruiz", "calle del olvido, 1", 654712330, "adan87grrr@mail.com"),
-("Maria", "Lopez", "Perez", "avenida buena vista, 33", 675000981, "lamarilo@mail.com"),
-("Lola", "Diaz", "Adalbert", "calle cristo redentor, 46 2ºb", 700123665, "lolitalamasbonita@mail.com");
+-- Datos de la tabla mascota
+INSERT INTO mascota VALUES
+("Angy", "perro", "chihuahua", "hembra", "marron", '2011-10-17', 05674382),
+("Lily", "perro", "bulldog frances", "hembra", "blanco y negro", '2015-04-01', 03748519),
+("Georgia", "perro", "chihuahua", "hembra", "blanco y marron", '2019-11-15', 02391647),
+("Gnar", "gato", "birmano", "macho", "beis y marron", '2021-03-23', 05862184),
+("Coco", "dragon barbudo", "pogona", "hembra", "crema", '2018-09-12', 02648391),
+("Peluso", "huron", "americano", "macho", "beis", '2008-07-03', 03657789), 
+("Mikita", "huron", "americano", "hembra", "marron oscuro", '2005-08-19', 03657789),
+("Cersey", "gato", "persa", "hembra", "beis", '2020-12-05', 05862184);
 
 INSERT INTO citas
