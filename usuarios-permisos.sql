7938db2f963a42ff31e751ad31237aecdba88bf1
Mikkita1 <152536723+Mikkita1@users.noreply.github.com>
2024-04-25 23:35:05 +0200

    Update usuarios-permisos.sql

--- usuarios-permisos.sql
+++ usuarios-permisos.sql
@@ -1,1 +1,5 @@
 -- Define qué usuarios crearías para que puedan acceder al SGBD y qué permisos tendrán.
+
+-- Crearia el usuario de uno de los clientes y le daria privilegios para insertar citas
+CREATE USER 'LuisGM'@'localhost' IDENTIFIED BY 'LuisGM';
+GRANT INSERT ON citas.CitasVeterinarias TO LuisGM@localhost;
