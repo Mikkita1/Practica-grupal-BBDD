169105d28a54b67c2239dc63190c88f161886df9
Mikkita1 <152536723+Mikkita1@users.noreply.github.com>
2024-04-25 23:29:30 +0200

    Update vistas.sql

--- vistas.sql
+++ vistas.sql
@@ -1,1 +1,8 @@
 -- ¿Qué vistas le proporcionaríais a cada usuario teniendo en cuenta las funciones que van a desarrollar? crea mínimo 2 vistas.
+
+CREATE VIEW mascota_cliente AS
+    SELECT * FROM mascota m 
+    INNER JOIN cliente cl ON cl.dni = m.dni GROUP BY dni;
+
+CREATE VIEW citas2024 AS
+    SELECT * citas WHERE fecha='2024%';
