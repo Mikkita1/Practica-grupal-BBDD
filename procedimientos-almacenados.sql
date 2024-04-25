0f8306979af4b4f39f45c458016915ecfbf5b9bf
Mikkita1 <152536723+Mikkita1@users.noreply.github.com>
2024-04-25 23:53:33 +0200

    Update procedimientos-almacenados.sql

--- procedimientos-almacenados.sql
+++ procedimientos-almacenados.sql
@@ -1,1 +1,13 @@
 -- ¿Qué procedimientos almacenados podría necesitar la base de datos?, crea 2 procedimientos almacenados.
+
+DELIMITER //
+CREATE PROCEDURE cambiar_genero(p_genero varchar(10))
+BEGIN 
+    DECLARE genero char;
+    IF p_genero="hembra" THEN
+        SET genero='H';
+    IF p_genero="macho" THEN
+        SET genero='M';
+    END IF;
+END; //
+DELIMITER;
